897d14f014f9a051b47795c0a51b6efbc2d45e57
Michael Lawson <mdlawson@gmx.com>
2018-08-09 22:18:14 +0100

    Bundle size optimizations

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,13 @@
 const { injectBabelPlugin, compose } = require("react-app-rewired");
+const webpack = require("webpack");
 
 const {
   rewireWebpack: rewireTypescript,
   rewireJest: rewireTypescriptJest,
   rewireTSLint,
 } = require("react-app-rewire-typescript-babel-preset");
+
+const injectBundleAnalyzer = require("react-app-rewire-webpack-bundle-analyzer");
 
 const rewireEmotion = (config, env) =>
   injectBabelPlugin(
@@ -18,11 +21,39 @@ const rewireEmotion = (config, env) =>
 const rewireHot = (config, env) =>
   env === "production" ? config : injectBabelPlugin("react-hot-loader/babel", config);
 
+const rewireGraphQLTag = (config, env) =>
+  env === "production" ? injectBabelPlugin("babel-plugin-graphql-tag", config) : config;
+
+const rewireBundleAnalyzer = (config, env) =>
+  env === "production"
+    ? injectBundleAnalyzer(config, env, {
+        analyzerMode: "static",
+        openAnalyzer: false,
+      })
+    : config;
+
+const rewireRemovePolyfills = (config, env) => {
+  if (env !== "production") {
+    return config;
+  }
+
+  config.externals = {
+    "@firebase/polyfill": "window",
+    "whatwg-fetch": "fetch",
+    promise: "Promise",
+  };
+
+  return config;
+};
+
 module.exports = {
   webpack: compose(
     rewireTypescript,
     rewireEmotion,
+    rewireGraphQLTag,
+    // rewireRemovePolyfills,
     rewireHot,
+    rewireBundleAnalyzer,
   ),
   jest: rewireTypescriptJest,
 };
--- src/apollo/firebase.ts
+++ src/apollo/firebase.ts
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/database";
+
 import { createRtdbLink } from "apollo-link-firebase";
 
 if (firebase.apps.length === 0) {
